fb2975c3d01ed59e0b41ec00be42a09524006f71
rajpirathap <rajpirathap@gmail.com>
2019-03-17 11:36:19 +0800

    refactored code

--- codetest-1/src/main/java/com/raj/demo/controller/PartnerController.java
+++ codetest-1/src/main/java/com/raj/demo/controller/PartnerController.java
@@ -1,10 +1,12 @@
 package com.raj.demo.controller;
 
 import java.text.DecimalFormat;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.raj.demo.repository.PartnerRepository;
 import com.raj.demo.service.PartnerService;
@@ -27,8 +29,9 @@ public class PartnerController {
 	 * @param sellamt
 	 * @return
 	 */
-	@RequestMapping(value = "/getrate", method = RequestMethod.GET)
-	public String solveLevel2(@RequestParam("sellamt") Double sellamt) {
+	@RequestMapping(value = "/getrate", method = RequestMethod.POST)
+	public String solveLevel2(HttpServletRequest request, HttpServletResponse response, Model model) {
+		double sellamt = Double.parseDouble(request.getParameter("sellamt"));
 		double rate = service.getExchangeRateForScenario2(sellamt);
 
 		System.out.println(df.format(rate));
